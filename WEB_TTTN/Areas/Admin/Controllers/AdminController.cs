2f1f1edb26a2a267bde81f635068f174930dd470
Xuân Khoa <vukhoa20122003@gmail.com>
2023-10-03 21:01:47 +0700

    giao dien admin

---
+++ WEB_TTTN/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_TTTN.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
