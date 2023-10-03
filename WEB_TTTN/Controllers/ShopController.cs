a6c8574d5888d3378964308ac42c7e25f122de4d
Xuân Khoa <vukhoa20122003@gmail.com>
2023-10-03 13:56:48 +0700

    Giao dien

---
+++ WEB_TTTN/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_TTTN.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult ShopView()
+        {
+            return View();
+        }
+    }
+}
